a633cbb4dbdcb20c79760f222e17b8c4b9580be6
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-09-05 12:24:11 -0600

    Add files via upload

---
+++ ejercicio2examenprueba.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+//Programa que te da la tabla de multiplicar de un número
+int main(){
+	int i=1;
+	while (i==1)
+		{
+		int c;
+		printf("¿Que tabla de multiplicar quieres?: \n");
+		scanf("%d", &c);
+	
+		switch(c)
+			{
+		
+				case 1:
+					printf("Tabla del 1\n");
+					printf("1*1=1\n");
+					printf("1*2=2\n");
+					printf("1*3=3\n");
+					printf("1*4=4\n");
+					printf("1*5=5\n");
+					printf("1*6=6\n");
+					printf("1*7=7\n");
+					printf("1*8=8\n");
+					printf("1*9=9\n");
+					printf("1*10=10\n");
+				break;
+		
+				case 2:
+					printf("Tabla del 2\n");
+					printf("2*1=2\n");
+					printf("2*2=4\n");
+					printf("2*3=6\n");
+					printf("2*4=8\n");
+					printf("2*5=10\n");
+					printf("2*6=12\n");
+					printf("2*7=14\n");
+					printf("2*8=16\n");
+					printf("2*9=18\n");
+					printf("2*10=20\n");
+				break;
+		
+				case 3:
+					printf("Tabla del 3\n");
+					printf("3*1=3\n");
+					printf("3*2=6\n");
+					printf("3*3=9\n");
+					printf("3*4=12\n");
+					printf("3*5=15\n");
+					printf("3*6=18\n");
+					printf("3*7=21\n");
+					printf("3*8=24\n");
+					printf("3*9=27\n");
+					printf("3*10=30\n");
+				break;
+		
+				case 4:
+					printf("Tabla del 4\n");
+					printf("4*1=4\n");
+					printf("4*2=8\n");
+					printf("4*3=12\n");
+					printf("4*4=16\n");
+					printf("4*5=20\n");
+					printf("4*6=24\n");
+					printf("4*7=28\n");
+					printf("4*8=32\n");
+					printf("4*9=36\n");
+					printf("4*10=40\n");
+				break;
+		
+				case 5:
+					printf("Tabla del 5\n");
+					printf("5*1=5\n");
+					printf("5*2=10\n");
+					printf("5*3=15\n");
+					printf("5*4=20\n");
+					printf("5*5=25\n");
+					printf("5*6=30\n");
+					printf("5*7=35\n");
+					printf("5*8=40\n");
+					printf("5*9=45\n");
+					printf("5*10=50\n");
+				break;
+				
+				case 6:
+					printf("Tabla del 6\n");
+					printf("6*1=6\n");
+					printf("6*2=12\n");
+					printf("6*3=18\n");
+					printf("6*4=24\n");
+					printf("6*5=30\n");
+					printf("6*6=36\n");
+					printf("6*7=42\n");
+					printf("6*8=48\n");
+					printf("6*9=54\n");
+					printf("6*10=60\n");
+				break;
+				
+				case 7:
+					printf("Tabla del 7\n");
+					printf("7*1=7\n");
+					printf("7*2=14\n");
+					printf("7*3=21\n");
+					printf("7*4=28\n");
+					printf("7*5=35\n");
+					printf("7*6=42\n");
+					printf("7*7=49\n");
+					printf("7*8=56\n");
+					printf("7*9=63\n");
+					printf("7*10=70\n");
+				break;
+				
+				case 8:
+					printf("Tabla del 8\n");
+					printf("8*1=8\n");
+					printf("8*2=16\n");
+					printf("8*3=24\n");
+					printf("8*4=32\n");
+					printf("8*5=40\n");
+					printf("8*6=48\n");
+					printf("8*7=56\n");
+					printf("8*8=64\n");
+					printf("8*9=72\n");
+					printf("8*10=80\n");
+				break;
+				
+				case 9:
+					printf("Tabla del 9\n");
+					printf("9*1=9\n");
+					printf("9*2=18\n");
+					printf("9*3=27\n");
+					printf("9*4=36\n");
+					printf("9*5=45\n");
+					printf("9*6=54\n");
+					printf("9*7=63\n");
+					printf("9*8=72\n");
+					printf("9*9=81\n");
+					printf("9*10=90\n");
+				break;
+		
+				default:
+					printf("Tabla del 10\n");
+					printf("10*1=10\n");
+					printf("10*2=20\n");
+					printf("10*3=30\n");
+					printf("10*4=40\n");
+					printf("10*5=50\n");
+					printf("10*6=60\n");
+					printf("10*7=70\n");
+					printf("10*8=80\n");
+					printf("10*9=90\n");
+					printf("10*10=100\n");
+					i=6;
+				break;
+			
+			}
+	
+		}
+	
+	
+	return 0;
+	
+	
+}
